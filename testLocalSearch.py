092d1d8e81d7c6f53bd4e9b1a172ab1aa94f6212
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2022-04-29 16:01:11 -0400

    Update to reflect changes to LocalSearch class and test get_random_successor
    Co-authored-by: Artjom-Plaunov <artyemnyc@gmail.com>

--- testLocalSearch.py
+++ testLocalSearch.py
@@ -5,12 +5,19 @@ if __name__ == "__main__":
     b = Board()
     b.parse_terrain("terrain.txt")
     # Setup blue
-    blue_config = LocalSearch().get_random_start(b, (0,11))
-    print(blue_config)
+    blue_local_search = LocalSearch(b, (0,11))
+    blue_config = blue_local_search.get_random_start()
+    # print(blue_config)
     print(b.place_pieces("blue", blue_config))
     # Setup brown
-    print(b.place_pieces("brown", LocalSearch().get_random_start(b, (12,23))))
-    print(b.blue_pieces)
-    print(b.blue_pieces_locations)
-    print(b.blue_piece_counts)
+    print(b.place_pieces("brown", LocalSearch(b, (12,23)).get_random_start()))
+    # print(b.blue_pieces)
+    # print(b.blue_pieces_locations)
+    # print(b.blue_piece_counts)
+    print(len(blue_local_search.get_successor_states(blue_config)))
+    successor = blue_local_search.get_random_successor(blue_config)
+    for k,v in blue_config.items():
+        if successor[k] != blue_config[k]:
+            print(k, v)
+            print(k, successor[k])
     b.display()
