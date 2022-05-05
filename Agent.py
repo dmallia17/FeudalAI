305c283ac7cd61a57b390bd63404fc9a1c5a2cf7
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2022-05-05 19:01:32 -0400

    Expand Agent.py to handle piece placement
    Co-authored-by: Artjom-Plaunov <artyemnyc@gmail.com>

--- Agent.py
+++ Agent.py
@@ -1,13 +1,30 @@
+from LocalSearch import *
 
+local_search_agents = {
+    "HillClimbingGreedy" : HillClimbingGreedy,
+    "HillClimbingFirstChoice" : HillClimbingFirstChoice,
+    "SimulatedAnnealing" : SimulatedAnnealing
+}
 
 class Agent():
     # How to handle variable arguments
-    def __init__(self, color, time_limit):
+    def __init__(self, color, time_limit, local_search_method,
+        local_search_init_args, local_search_run_args):
         self.color = color
         self.time_limit = time_limit
+        self.local_search_method = local_search_agents[
+            local_search_method](**local_search_init_args)
+        self.local_search_run_args = local_search_run_args # Dictionary
 
     # @return   A list of (origin, new location) pairs
     def get_choice(self, board):
+        pass
+
+    def get_piece_placement(self, board):
+        return self.local_search_method.get_piece_placement(
+            **self.local_search_run_args)
+
+    def get_statistics(self):
         pass
 
 class HumanAgent(Agent):
