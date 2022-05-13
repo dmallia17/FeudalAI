d6131c32bfacfc5290fee3ac4cd449173b5f9875
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2022-05-12 22:20:54 -0400

    Artjom: update testSimulatedAnnealing to new get_piece_placement signature and print time elapsed
    Co-authored-by: Artjom-Plaunov <artyemnyc@gmail.com>

--- testSimulatedAnnealing.py
+++ testSimulatedAnnealing.py
@@ -1,14 +1,15 @@
 from Board import *
 from LocalSearch import *
+import time
 
 if __name__ == "__main__":
     b = Board()
-    b.parse_terrain("terrain.txt")
+    b.parse_terrain("terrain_3M_official_1.txt")
 
+    prev_time = time.process_time()
     blue_local_search = SimulatedAnnealing(b, "blue")
-    blue_config = blue_local_search.get_random_start()
-
-    new_config = blue_local_search.get_piece_placement(blue_config,10,.99)
+    new_config = blue_local_search.get_piece_placement(10,.99)
+    print(time.process_time() - prev_time)
 
     b.place_pieces("blue", new_config)
     b.display()
