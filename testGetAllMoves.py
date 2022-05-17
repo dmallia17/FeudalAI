8cb2de786c38a21ccf94b29ca5a3f7559ac80727
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2022-05-17 18:45:20 -0400

    Artjom: adjustment to test script for retrieving move counts
    Co-authored-by: Artjom-Plaunov <artyemnyc@gmail.com>

--- testGetAllMoves.py
+++ testGetAllMoves.py
@@ -3,36 +3,42 @@ from LocalSearch import *
 from time import perf_counter
 
 if __name__ == "__main__":
-    b3 = Board()
-    b3.parse_terrain("terrain_3M_official_1.txt")
-    blue_searcher = HillClimbingFirstChoice(b3, "blue")
-    brown_searcher = HillClimbingFirstChoice(b3, "brown")
-    blue_random = blue_searcher.get_random_start()
-    brown_random = brown_searcher.get_random_start()
-    b3.place_pieces("blue", blue_random)
-    b3.place_pieces("brown", brown_random)
-    b3.display()
-    
-    start = perf_counter()
-    count = 0
-    for m in b3.get_all_moves_ref("brown"):
-        count += 1
-    end = perf_counter()
-    print("Time elapsed (get_all_moves_ref): ", (end-start))
-    print("num moves generated: ", count)
-    print()
 
-    start = perf_counter()
-    count = 0
-    for m in b3.get_all_moves("brown"):
-        count += 1
-    end = perf_counter()
-    print("Time elapsed (get_all_moves): ", (end-start))
-    print("num moves generated: ", count)
-    print()
+    res = 0
 
-    start = perf_counter()
-    test = b3.get_num_all_moves("brown")
-    end = perf_counter()
-    print("Time elapsed (get_num_all_moves): ", (end-start))
-    print(test)
+    for i in range(1):
+        b = Board()
+        b.parse_terrain("terrain_3M_official_1.txt")
+        blue_searcher = HillClimbingFirstChoice(b, "blue")
+        brown_searcher = HillClimbingFirstChoice(b, "brown")
+        blue_random = blue_searcher.get_random_start()
+        brown_random = brown_searcher.get_random_start()
+        b.place_pieces("blue", blue_random)
+        b.place_pieces("brown", brown_random)
+        res += (b.get_num_all_moves("brown"))
+
+    print(res/1.0)
+
+    # start = perf_counter()
+    # count = 0
+    # for m in b3.get_all_moves_ref("brown"):
+    #     count += 1
+    # end = perf_counter()
+    # print("Time elapsed (get_all_moves_ref): ", (end-start))
+    # print("num moves generated: ", count)
+    # print()
+
+    # start = perf_counter()
+    # count = 0
+    # for m in b3.get_all_moves("brown"):
+    #     count += 1
+    # end = perf_counter()
+    # print("Time elapsed (get_all_moves): ", (end-start))
+    # print("num moves generated: ", count)
+    # print()
+
+    # start = perf_counter()
+    # test = b3.get_num_all_moves("brown")
+    # end = perf_counter()
+    # print("Time elapsed (get_num_all_moves): ", (end-start))
+    # print(test)
